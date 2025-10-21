027c06dea59db43f0e0da488c50ea45e9839dd98
Masa Matsuoka <masa@Masas-Studio.local>
2025-10-21 16:28:54 -0400

    style: fix Black formatting in utils __init__.py

--- src/gitflow_analytics/utils/__init__.py
+++ src/gitflow_analytics/utils/__init__.py
@@ -2,4 +2,4 @@
 
 from .commit_utils import get_parent_count, is_initial_commit, is_merge_commit
 
-__all__ = ['is_merge_commit', 'get_parent_count', 'is_initial_commit']
+__all__ = ["is_merge_commit", "get_parent_count", "is_initial_commit"]
